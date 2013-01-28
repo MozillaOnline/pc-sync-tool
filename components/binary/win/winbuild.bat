4f20e0e2a0bba6a680f21752490c8a7a3d0bf5ba
pzhang <pzhang@mozilla.com>
2013-01-28 17:11:20 +0800

    Update libs for Mac and Windows

---
+++ components/binary/win/winbuild.bat
@@ -0,0 +1,2 @@
+cl /c adbservice.c
+link /dll adbservice.obj
---
+++ components/binary/win/winsetenv.bat
@@ -0,0 +1,4 @@
+SET   PATH=D:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin;%PATH%
+SET   INCLUDE=D:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include;%INCLUDE%
+SET   LIB=D:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\lib;%LIB%
+"D:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
