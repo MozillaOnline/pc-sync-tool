623cfa5c629dc4aba0586fc156bedb394cbb7f19
DongSheng Xue <dxue@mozilla.com>
2014-07-17 09:20:27 +0800

    remove dom.mozTCPSocket.enabled

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,3 @@
-// Open mozTCPSocket
-pref('dom.mozTCPSocket.enabled', true);
-
 pref('extensions.ffosassistant@mozillaonline.com.firstrun', true);
 pref('extensions.ffosassistant@mozillaonline.com.manuallyRemovedButton', false);
 pref('extensions.ffosassistant@mozillaonline.com.pinnedOnOpen', true);
